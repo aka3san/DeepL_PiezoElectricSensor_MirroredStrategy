ca3cefc263b47619f06c9aa685da18800b37f1d1
aka3san <tzmtdustnsk.azs@gmail.com>
2023-07-25 19:12:52 +0900

    strategyをコメントアウト。

--- deepL.py
+++ deepL.py
@@ -12,10 +12,14 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 from skopt import gp_minimize
 from bayes_opt import BayesianOptimization
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
+import prpl
 
 #csvを受け取って、トレーニングを行う。
 def deepL_keras(csv, dls, num_epoch, batch, plot=True, k_fold=False):
+    tf.keras.backend.clear_session()
     #データの定義
     X = csv.iloc[:, 0:dls.num_featureValue]
     y = csv.iloc[:, dls.num_featureValue:len(csv.columns)]
@@ -23,7 +27,7 @@ def deepL_keras(csv, dls, num_epoch, batch, plot=True, k_fold=False):
     X_test = X[dls.trainRow[1]:len(X)]
     y_train = y[dls.trainRow[0]:dls.trainRow[1]]
     y_test = y[dls.trainRow[1]:len(X)]
-
+    #with dls.strategy.scope():     
     history = dls.model.fit(X_train, y_train, batch_size=batch, epochs=num_epoch, verbose=1)
     score = dls.model.evaluate(X_test, y_test, verbose=0)
     
@@ -49,7 +53,7 @@ def deepL_keras(csv, dls, num_epoch, batch, plot=True, k_fold=False):
 dls = DeepLSetting()
 dls.set_initial(40,2,[0,42])
 #data = pd.read_csv("/home/k_inayoshi/DeepL_PiezoElectricSensor/TrainingData/out0626.csv")
-data = pd.read_csv("/Users/inayoshikoya/Library/CloudStorage/OneDrive-国立大学法人東海国立大学機構/ドキュメント/DeepL_PiezoElectricSensor/TrainingData/out0626.csv")
+data = pd.read_csv("./TrainingData/out0626.csv")
 #データを正規化
 data["H"] = data["H"]/10
 data["H/A'B'"] = data["H/A'B'"]/100
@@ -61,7 +65,7 @@ pbounds = {
         'dropout': (0.1,0.4),
         'batch': (2,10)}
 options = {'c1': 0.8, 'c2': 0.8, 'w': 0.2, 'k': 3, 'p': 2}
-dls.psoOpt(data, 5000, 100)
+dls.psoOpt(data, 1000, 100)
 #dls.bayesOpt(data , pbounds=pbounds, num_epoch=5000)
 """
 
@@ -71,12 +75,12 @@ dls.set_initial(40,2,[0,39])
 dls.set_modelLayerAndNode([40,512,512,2], dropout=0.2)
 dls.model_compile()
 dls.model.summary()
-data = pd.read_csv("TrainingData/out0626.csv")
+data = pd.read_csv("./TrainingData/out0626.csv")
 #データを正規化
 data["H"] = data["H"]/10
 data["H/A'B'"] = data["H/A'B'"]/100
 data["SBP"] = data["SBP"]/200
 data["DBP"] = data["DBP"]/200
-deepL_keras(data, dls, 10000, 10, plot=True)#k_fold=5)
+deepL_keras(data, dls, 5000, 10, plot=True)#k_fold=5)
 
 
--- deepLSetting.py
+++ deepLSetting.py
@@ -9,6 +9,13 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 import pyswarms as ps
 from pyswarms.utils.search import GridSearch
+import tqdm
+import multiprocessing
+from pathos.multiprocessing import ProcessingPool
+import prpl
+import pandas as pd
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 #機械学習のハイパーパラメータ、学習方法などをまとめたクラス。
 class DeepLSetting:
     
@@ -16,9 +23,9 @@ class DeepLSetting:
         self.num_featureValue = None #特徴量の数
         self.num_output = None #目的変数の数
         self.trainRow = None #学習データの割合
-        self.strategy = tf.distribute.MirroredStrategy() #分散ストラテジーの作成
-        with self.strategy.scope():
-            self.model = tf.keras.Sequential()#学習モデル。
+        #self.strategy = tf.distribute.MirroredStrategy() #分散ストラテジーの作成
+        #with self.strategy.scope():
+        self.model = tf.keras.Sequential()#学習モデル。
         self.num_nodeList = None #レイヤーの配列。
     
     #説明変数の数、目的変数の数、学習データの割合を設定する。
@@ -29,31 +36,36 @@ class DeepLSetting:
     
         
     #modelにNN構造を手動で設定する
-    def set_modelLayerAndNode(self,num_nodeList: list, activation="relu", dropout=0.2):
+    def set_modelLayerAndNode(self, num_nodeList: list, activation="relu", dropout=0.2):
         self.num_nodeList = num_nodeList.copy()
-        with self.strategy.scope():
-            self.model.add(tf.keras.layers.Dense(num_nodeList[1], input_shape=(num_nodeList[0],)))
+        #with self.strategy.scope():
+        self.model.add(tf.keras.layers.Dense(num_nodeList[1], input_shape=(num_nodeList[0],)))
+        #self.model.add(Activation(activation))
+        self.model.add(tf.keras.layers.Dropout(dropout))
+        if(len(num_nodeList) == 2):
+            return
+        for i in range(2, len(num_nodeList)):
+            self.model.add(tf.keras.layers.Dense(num_nodeList[i]))
             #self.model.add(Activation(activation))
-            self.model.add(tf.keras.layers.Dropout(dropout))
-            if(len(num_nodeList) == 2):
-                return
-            for i in range(2, len(num_nodeList)):
-                self.model.add(tf.keras.layers.Dense(num_nodeList[i]))
-                #self.model.add(Activation(activation))
-                if i < len(num_nodeList)-1:
-                    self.model.add(tf.keras.layers.Dropout(dropout))
+            if i < len(num_nodeList)-1:
+                self.model.add(tf.keras.layers.Dropout(dropout))
 
     #モデルをコンパイルする。
     def model_compile(self, loss_tmp='mean_squared_error', optimizer_tmp=tf.keras.optimizers.Adam(), metrics_tmp=["mae"],run_eagerly=True):
-        with self.strategy.scope():
-            self.model.compile(
-            loss = loss_tmp,
-            optimizer=optimizer_tmp,
-            metrics=metrics_tmp,
-            experimental_run_tf_function=False)
-
+        #with self.strategy.scope():
+        self.model.compile(
+        loss = loss_tmp,
+        optimizer=optimizer_tmp,
+        #metrics=metrics_tmp 
+        )
+        
+    @tf.function(reduce_retracing=True)
+    def model_fit(self, X_train, y_train, batch, num_epoch, iter):
+        self.model.fit(X_train, y_train, batch_size=int(batch), initial_epoch=iter*num_epoch, epochs=(iter+1)*num_epoch, verbose=0)
+        
     #ベイズ最適化用の関数
-    def func(self, num_layer, num_node, dropout, batch, data=None, num_epoch=3000, loss_tmp='mean_squared_error', optimizer_tmp=tf.keras.optimizers.Adam(), k_fold=0):
+    def func(self, num_layer, num_node, dropout, batch, data=None, num_epoch=3000, loss_tmp='mean_squared_error', optimizer_tmp=tf.keras.optimizers.Adam(), iter=0, k_fold=0):
+        tf.keras.backend.clear_session()
         data_tmp = data.copy()
         nodeList = [self.num_featureValue]
         for i in range(1, int(num_layer)):
@@ -61,10 +73,10 @@ class DeepLSetting:
                 nodeList.append(self.num_output)
                 break
             nodeList.append(int(num_node))
-        with self.strategy.scope():
-            self.model = tf.keras.Sequential() #モデルの初期化
-            self.set_modelLayerAndNode(nodeList, dropout=dropout) #モデル構造の定義
-            self.model_compile(loss_tmp=loss_tmp, optimizer_tmp=optimizer_tmp)
+        #with self.strategy.scope():
+        self.model = tf.keras.Sequential() #モデルの初期化
+        self.set_modelLayerAndNode(nodeList, dropout=dropout) #モデル構造の定義
+        self.model_compile(loss_tmp=loss_tmp, optimizer_tmp=optimizer_tmp)
         #データの準備
         X = data_tmp.iloc[:, 0:self.num_featureValue]
         y = data_tmp.iloc[:, self.num_featureValue:len(data_tmp.columns)]
@@ -76,15 +88,23 @@ class DeepLSetting:
         if k_fold:
             return -40000*self.k_foldCrossValidation(k_fold, X_train, y_train, nodeList, dropout, num_epoch=num_epoch, batch=int(batch), loss_tmp=loss_tmp, optimizer_tmp=optimizer_tmp)
         #学習
-        with self.strategy.scope():
-            history = self.model.fit(X_train, y_train, batch_size=int(batch), epochs=num_epoch, verbose=0)
-            score = self.model.evaluate(X_test, y_test, verbose=0)
-        return -40000*score[0]
+        #with self.strategy.scope():
+        history = self.model.fit(X_train, y_train, batch_size=int(batch), initial_epoch=iter*num_epoch, epochs=(iter+1)*num_epoch, verbose=0)
+        score = self.model.evaluate(X_test, y_test, verbose=0)
+        return -40000*score
+   
+    #pso最適化用関数内で使用するmultiprocessing用の関数
+    def func_ps_mulpro(self, iter, x=None, data=None, num_epoch=3000, loss_tmp='mean_squared_error', optimizer_tmp=tf.keras.optimizers.Adam()):
+        score = -1*self.func(x[iter,0], x[iter,1], x[iter,2], x[iter, 3], data=data, num_epoch=num_epoch, loss_tmp=loss_tmp, optimizer_tmp=optimizer_tmp)
+        print(f"score:{score}")
+        return score
+   
+    #pso最適化用の関数
     
     def func_ps(self, x, data=None, num_epoch=3000, loss_tmp='mean_squared_error', optimizer_tmp=tf.keras.optimizers.Adam()):
         y = []
         for i in range(len(x)):
-            score = -1*self.func(x[i,0], x[i,1], x[i,2], x[i, 3], data=data, num_epoch=num_epoch, loss_tmp=loss_tmp, optimizer_tmp=optimizer_tmp)
+            score = -1*self.func(x[i,0], x[i,1], x[i,2], x[i, 3], data=data, num_epoch=num_epoch, loss_tmp=loss_tmp, optimizer_tmp=optimizer_tmp, iter=i)
             y.append(score)
         return y
 
@@ -93,7 +113,7 @@ class DeepLSetting:
     def bayesOpt(self, data, pbounds,num_epoch=3000, n_iter=25, loss_tmp='mean_squared_error', optimizer_tmp=tf.keras.optimizers.Adam(), k_fold=0):
         if(self.num_featureValue == None):
             raise Exception("先にset_initial関数で初期化してください。")
-        self.func.__func__.__defaults__ = data, num_epoch, loss_tmp, optimizer_tmp, k_fold
+        self.func.__func__.__defaults__ = data, num_epoch, loss_tmp, optimizer_tmp, 0, k_fold, 
         optimizer = BayesianOptimization(f=self.func, pbounds=pbounds)
         optimizer.maximize(init_points=5, n_iter=n_iter)
     #NN構造をレイヤー数、ノード数、ドロップアウト、バッチ数を最適化する。(PSO最適化)
@@ -109,7 +129,7 @@ class DeepLSetting:
         #print(f"best_options: {best_options}")
         data = data.copy()
         options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9, 'k': 3, 'p': 2} #パラメータを設定
-        bounds=((3, 2, 0.1, 1), (10, 4096, 0.4, 5)) #範囲を指定
+        bounds=((3, 2, 0.1, 2), (10, 2048, 0.4, 40)) #範囲を指定
         optimizer = ps.single.LocalBestPSO(n_particles=5, dimensions=4, options=options, bounds=bounds)
         cost, pos = optimizer.optimize(self.func_ps, n_iter, verbose=1)
         f = open('sample.txt', 'w', encoding='UTF-8')
@@ -140,3 +160,22 @@ class DeepLSetting:
         return ave_all_test_loss
 
 
+if __name__ == '__main__':
+    from multiprocessing import freeze_support
+    freeze_support()
+    dls = DeepLSetting()
+    dls.set_initial(40,2,[0,42])
+    #data = pd.read_csv("/home/k_inayoshi/DeepL_PiezoElectricSensor/TrainingData/out0626.csv")
+    data = pd.read_csv("./TrainingData/out0626.csv")
+    #データを正規化
+    data["H"] = data["H"]/10
+    data["H/A'B'"] = data["H/A'B'"]/100
+    data["SBP"] = data["SBP"]/200
+    data["DBP"] = data["DBP"]/200
+    pbounds = {
+            'num_layer': (3,7),
+            'num_node': (1,4096),
+            'dropout': (0.1,0.4),
+            'batch': (2,10)}
+    options = {'c1': 0.8, 'c2': 0.8, 'w': 0.2, 'k': 3, 'p': 2}
+    dls.bayesOpt(data, pbounds, 10000, 100)
